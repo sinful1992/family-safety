5a34f96546e347422aa71c6aa5030dca5530344f
sinful1992 <sinful1992@users.noreply.github.com>
2026-04-21 21:46:13 +0100

    fix(android): update MainApplication for RN 0.85 bridgeless architecture
    - Use OpenSourceMergedSoMapping for SoLoader (RN 0.76+ merged .so files)
    - Add getReactHost() override required by bridgeless/new architecture

--- android/app/src/main/java/com/familysafety/MainApplication.java
+++ android/app/src/main/java/com/familysafety/MainApplication.java
@@ -3,11 +3,15 @@ package com.familysafety;
 import android.app.Application;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactHost;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactHost;
 import com.facebook.react.defaults.DefaultReactNativeHost;
+import com.facebook.react.soloader.OpenSourceMergedSoMapping;
 import com.facebook.soloader.SoLoader;
+import java.io.IOException;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -38,9 +42,18 @@ public class MainApplication extends Application implements ReactApplication {
   }
 
   @Override
+  public ReactHost getReactHost() {
+    return DefaultReactHost.getDefaultReactHost(getApplicationContext(), mReactNativeHost, null);
+  }
+
+  @Override
   public void onCreate() {
     super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
+    try {
+      SoLoader.init(this, OpenSourceMergedSoMapping.INSTANCE);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       DefaultNewArchitectureEntryPoint.load();
     }
